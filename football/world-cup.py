fb34a0d5c58f4ea6caf4939755151923deb5ed6d
pcresolver <steve@pcresolver.es>
2014-08-17 19:30:19 +0100

    Football

--- football/world-cup.py
+++ football/world-cup.py
@@ -13,7 +13,7 @@ team_group_a = set([])  # create empty set to store teams
 
 
 def init():
-    for i in range(teams_in_group):  # create first group
+    for i in range(teams_in_group):  # create first group by creating objects of class Team from team list
         name = eval("team" + str(i + 1))  # converts string to variable name
         a_team = Team(name)  # create team with name - name
         team_group_a.add(a_team)  # add a_team to team group
@@ -24,13 +24,11 @@ def find_element(search,
     # this can then be used to work out the result of the matches and build the table
     return [element for element in content if element[0] == search or element[2] == search]
 
-
 def get_fixtures():
     with open('fixtures.txt') as fixturesfile:
         fixtureslist = fixturesfile.read().split()
     print(fixtureslist)
     return fixtureslist
-
 
 def clean_up(fixtures_messy):
     # remove half time scores
@@ -40,35 +38,29 @@ def clean_up(fixtures_messy):
             fixtures_messy.remove(entry)
     return fixtures_messy
 
-
-def split_matches(fixtures_blob, n):  # split fixtures list into individual matches
+def split_matches(fixtures_blob, n):  # split fixtures list into individual matches by splitting into list of elements of n strings
     # I expect n to be 3
     if n < 1:
         n = 1
     fixtures_split = [fixtures_blob[i:i + n] for i in range(0, len(fixtures_blob), n)]
-    # print("SCORES are :", fixtures_split)
-    home_goals = fixtures_split[0][1][0]
-    # print("Home Goals: ", home_goals)
-    results_copy = fixtures_split
-    for result in fixtures_split:
+    for result in fixtures_split:  # takes each result and replaces "0:1" with '0' and adds '1' to end of list
         if str(result[1]) == "-":
             pass
-            # print(result[1])
         else:
-            home_goals = result[1][0]
-            away_goals = result[1][2]
-            result[1] = home_goals
-            result.append(away_goals)
-            # print("home goals:", home_goals, "away:", away_goals, "result: ", result)
-            # result.append(results_copy[][1][1])
-            # result[1] = results_copy[1][1][0]
-            # print("scores: ", result[1], result[3])
+            home_goals = result[1][0] # temp store
+            away_goals = result[1][2] # temp store
+            result[1] = home_goals  # replace "0:1" with '0'
+            result.append(away_goals)  # adds '1' to end of list
 
     return fixtures_split
 
 
-def process_results(results) -> object:
-    for result in results:
+def process_results(results) -> object: # using this to see if I can add goals up. so far only works on first result
+    total_goals = 0
+    for match_result in results:
+        print(match_result)
+        total_goals += int(match_result[1]) + int(match_result[3])
+        print("goals so far: ", total_goals)
         return
     return
 
@@ -83,19 +75,16 @@ class Team:
         return self.name
 
 
-class Result:
-    def __init__(self, name):
-        self.name = name
-        return
-
-
-        #
+# class Result:
+#     def __init__(self, name):
+#         self.name = name
+#         return
 
 # Let's get started
 init()
 fixtures = get_fixtures()
 results = clean_up(fixtures)
-print("fixtures before results are split is called: ", fixtures)
+print("Fixtures before results are split is called: ", fixtures)
 results = split_matches(results, 3)
-print("these are the results so far ", results)
+print("These are the results so far ", results)
 process_results(results)
